93579a5604fe7cb8d81fa0e355bb965fa8a8da90
Sebastianlr11 <sebastianlr051@gmail.com>
2025-02-21 16:23:10 -0500

    version 1.7

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import daisyui from 'daisyui';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()] 
+    
   }
 });
